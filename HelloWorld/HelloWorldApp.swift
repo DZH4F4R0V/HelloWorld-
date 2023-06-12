8d77ac7ce4884f4f83a4f6de642c5733cfbcbea2
DZH4F4R0V <jeff-122@ya.ru>
2023-06-12 14:30:33 +0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by J Eff on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
